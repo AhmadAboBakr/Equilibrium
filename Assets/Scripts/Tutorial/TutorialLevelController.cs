24d00b3681bda487aebfb3b4e212991ed128188a
unknown <ahmadabobakr@gmail.com>
2015-05-25 16:41:00 +0200

    Started reimplementing tutorial

---
+++ Assets/Scripts/Tutorial/TutorialLevelController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class TutorialLevelController : MonoBehaviour
+{
+    public static TutorialLevelController instance; 
+    public List<GameObject> stages;
+    int currentState = 0;
+    void Awake()
+    {
+        instance = this;
+    }
+    void Start()
+    {
+        foreach (var stage in stages)
+        {
+            stage.gameObject.SetActive(false);
+        }
+        stages[currentState].SetActive(true);
+
+    }
+    public void StartNextSegment()
+    {
+        stages[currentState++].SetActive(false);
+        stages[currentState].SetActive(true);
+    }
+
+}
---
+++ Assets/Scripts/Tutorial/TutorialStage.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialStage : MonoBehaviour {
+    public GameObject CompanioinUIElement;
+    void OnEneable()
+    {
+        if (CompanioinUIElement)
+        {
+            CompanioinUIElement.SetActive(true);
+        }
+    }
+    void OnDisable()
+    {
+        if (CompanioinUIElement)
+        {
+            CompanioinUIElement.SetActive(false);
+        }
+
+
+    }
+    void OnTriggerEnter2D()
+    {
+        TutorialLevelController.instance.StartNextSegment();
+    }
+}
