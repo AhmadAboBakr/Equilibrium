c58cb2010743a15c929845eba23732d06c0f7dd1
Hossam Shukrallah <hossam.shuk@gmail.com>
2015-06-04 19:36:37 +0200

    splash screen

---
+++ Assets/Scripts/UI/SplashLoadMainMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashLoadMainMenu : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void LoadMainMenu()
+    {
+        Application.LoadLevel("mainMenuScene");
+    }
+}
