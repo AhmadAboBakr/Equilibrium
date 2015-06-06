26200ddd8e6d207fffd2e8830d15cc27db96758f
unknown <ahmadabobakr@gmail.com>
2015-06-06 13:30:30 +0200

    OptionMenu Art

---
+++ Assets/Screenshots.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Screenshots : MonoBehaviour
+{
+    public int hamada;
+    // Use this for initialization
+    void Start()
+    {
+        hamada = 0;
+
+        //StartCoroutine(GetScreen());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public IEnumerator GetScreen()
+    {
+        while(true)
+        {
+            
+            hamada++;
+            Application.CaptureScreenshot("C:\\Users\\SDAndGaming\\Documents\\Bakr\\ssss\\Equilibrium\\Assets\\Hamada\\screenshot"+ hamada+".png");
+            yield return new WaitForSeconds(1.5f);
+        }
+    }
+}
